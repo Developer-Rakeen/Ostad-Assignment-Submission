414b51481f07560046ab75ce40c07a2bf3a5f52f
Dev Rakeen <mr.rakeen.bd@gmail.com>
2023-08-02 22:34:26 +0600

    Uploaded

--- module_1_assignment.dart
+++ module_1_assignment.dart
@@ -1,5 +1,5 @@
 void main() {
-  var a = 10, b = 5;
+  const int a = 10, b = 5;
   var add = a + b, sub = a - b, mul = a * b, div = a / b, mod = a % b;
   print('a = $a, b = $b\nAddition: $a + $b = $add\nSubtraction: $a - $b = $sub\nMultiplication: $a * $b = $mul\nDivision: $a / $b = $div\nModulus: $a % $b = $mod');
 }
